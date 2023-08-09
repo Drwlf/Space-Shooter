014a800626d58841f09fd64d98e797f9c2d31998
thomas mason <joeswanson6942@gmail.com>
2023-08-08 19:45:22 -0700

    User input changes

--- Assets/Scenes/Scripts/Player.cs
+++ Assets/Scenes/Scripts/Player.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]
+
+    private float speed = 3.5f;
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // take the current position = new position ( 0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+        transform.Translate(Vector3.up* verticalInput * speed * Time.deltaTime);
+
+       
     }
 }
