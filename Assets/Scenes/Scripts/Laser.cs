9cc75506faddf8c7ce2f23dcb8efad1d182d1500
thomas mason <joeswanson6942@gmail.com>
2023-08-10 20:33:50 -0700

    Laser added

---
+++ Assets/Scenes/Scripts/Laser.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed = 8.0f;
+
+    void Start()
+    {
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.up * _speed * Time.deltaTime);
+    }
+}
--- Assets/Scenes/Scripts/Player.cs
+++ Assets/Scenes/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
 
     private float speed = 3.5f;
     
+    [SerializeField]
+    private GameObject _laserPrefab;
 
 
     // Start is called before the first frame update
@@ -21,12 +23,36 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        CalculatedMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(_laserPrefab,transform.position, Quaternion.identity);
+            
+
+            
+
+        }
+    }
+        void CalculatedMovement()
+    {
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
-        transform.Translate(Vector3.up* verticalInput * speed * Time.deltaTime);
-
        
+
+        transform.Translate(new Vector3(horizontalInput, verticalInput, 0) * speed * Time.deltaTime);
+            
+        transform.position = new Vector3(transform.position.x, Mathf.Clamp(transform.position.y, -3.8f, 0), 0);
+
+        if (transform.position.x > 11.3f)
+        {
+            transform.position = new Vector3(-11.3f, transform.position.y, 0);
+        }
+        else if (transform.position.x < -11.3f)
+        {
+            transform.position = new Vector3(11.3f, transform.position.y, 0);
+        }
     }
+
 }
