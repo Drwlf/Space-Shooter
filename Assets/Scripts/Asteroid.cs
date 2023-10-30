ce40aac8915c4d6bb29063037e34f1992a2d5deb
Thomas Mason <joeswanson6942@gmail.com>
2023-10-29 20:41:23 -0700

    Last update

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _explosionPrefab;
+    private SpawnManager _spawnManager;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _spawnManager = GameObject.Find("Spawn_Manager").GetComponent<SpawnManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, 1.0f, Space.Self);
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Laser")
+        {
+            Instantiate(_explosionPrefab, transform.position, Quaternion.identity);
+            _spawnManager.StartSpawning();
+            Destroy(this.gameObject, 0.35f);
+
+        }
+            
+    }
+}
